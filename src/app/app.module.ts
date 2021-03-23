ada4399496f486a0fe2c1b038d333776e2f76d67
Sagar <sagarbelotes@gmail.com>
2021-03-23 13:55:54 +0530

    added one component and rendered

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestgitComponent } from './testgit/testgit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestgitComponent
   ],
   imports: [
     BrowserModule,
